b009470fb67a95de4cd27d386bfb8ae4dfc4e4d6
Shepel Pavel <pavel@shepel.net>
2022-12-25 13:11:23 +0300

    build

--- docs/_nuxt/static/1670100839/linux/install-astra/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670100839",layout:"default",error:a,serverRendered:true,routePath:"\u002Flinux\u002Finstall-astra",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e6a86419"}}}}(null));
---
+++ docs/_nuxt/static/1671963068/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1671963068",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"62824c51"}}}}(null,"\u002F"));
